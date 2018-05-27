7a7a0a7df4391dae3642d99cb3ccbff8f15bb1aa
Bara Abraham <barasec@protonmail.com>
2018-05-27 14:30:01 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+	@Override
+	public void start(Stage stage) throws Exception
+	{
+		Parent root = FXMLLoader.load(getClass().getResource("Resources/UI/MainUI.fxml"));
+
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.setTitle("Mapping Palestine");
+		stage.getIcons().add(new Image("file:src/resources/Images/MapIcon.png"));
+		stage.show();
+	}
+
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+}
